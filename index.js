eab23138729b6be975437e68da0f0da9e94e7317
Piotr Krasny <krasny.piotr@gmail.com>
2021-08-25 08:02:50 +0200

    Clean

--- index.js
+++ index.js
@@ -6,16 +6,19 @@ oraz kilku wbudowanych zmiennych globalnych.)
 const {join,basename, dirname, extname, normalize, resolve} = require('path');
 
 function program1() {
-//Ścieżka do katalogu w którym jest nasz entrypoint:
-    console.log(__dirname);
+    // Ścieżka do katalogu w którym jest nasz entrypoint:
+    // C:\Users\Piotr\Documents\Projects\p58megakex
+    console.log("__dirname", __dirname);
 
-//Ścieżka do pliku programu:
-    console.log(__filename);
+    //Ścieżka do pliku programu:
+    // C:\Users\Piotr\Documents\Projects\p58megakex\index.js
+    console.log("__filename", __filename);
 
-//Sama nazwa uruchamianego pliku lub katalogu. Bez wskazania czy to plik czy katalog:
+    //Sama nazwa uruchamianego pliku lub katalogu. Bez wskazania czy to plik czy katalog:
     myDirPath = './test'
-    console.log("basename:", basename(myDirPath));
-    console.log("basename:", basename(__filename));
+    console.log('myDirPath = \'./test\'');
+    console.log("basename(myDirPath):", basename(myDirPath));
+    console.log("basename(__filename):", basename(__filename));
 
     /*
     Nie powinniśmy budować ścieżek jakoś ręcznie, bo różne systemy różnie je kodują.
@@ -23,36 +26,27 @@ function program1() {
      */
 
     const fullPath = join(__dirname, __filename);
-    console.log(fullPath);
+    console.log("join(__dirname, __filename)", fullPath);
 
     /*
     dirname jest odwrotnością basename. Podaje ścieżkę do celu
     __filename i __dirname są zmiennymi globalnymi do tego dochodzą zaimportowane metody.
      */
-    console.log("dirname:", dirname(__filename));
-
-    /*
-
-     */
-    console.log("__filename", __filename);
-// C:\Users\Piotr\Documents\Projects\p58megakex\index.js
-
-    console.log("__dirname", __dirname);
-// C:\Users\Piotr\Documents\Projects\p58megakex
+    console.log("dirname(__filename):", dirname(__filename));
 
     console.log("basename(__dirname)", basename(__dirname));
-// p58megakex
+    // p58megakex
 
     console.log("basename(__filename)", basename(__filename));
-// index.js
+    // index.js
 
     console.log("dirname(__dirname)", dirname(__dirname));
-// C:\Users\Piotr\Documents\Projects
+    // C:\Users\Piotr\Documents\Projects
 
     console.log("dirname(__filename)", dirname(__filename));
-// C:\Users\Piotr\Documents\Projects\p58megakex
+    // C:\Users\Piotr\Documents\Projects\p58megakex
 }
-//program1()
+program1()
 
 function program2() {
     // Taki program będzie też podawał nieistniejące pliki i katalogi,
