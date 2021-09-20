c0e6bf9ef5e8184bb349e3bed9a5280de324f038
Piotr Krasny <krasny.piotr@gmail.com>
2021-09-20 22:54:29 +0200

    Save

--- index.js
+++ index.js
@@ -10,18 +10,19 @@ function program1() {
         err && console.log(err)
     }));
 }
+
 //program1()
 
 /*
     Tworzenie katalogu, wersja promisowa
- */
+*/
 
 function program2() {
-    (async ()=>{
+    (async () => {
         try {
-            await mkdirprom('./mega_k2')
+            await mkdirprom('./mega_k2');
         } catch(err) {
-            console.log(err)
+            console.log(err);
         }
     })()
 }
@@ -32,10 +33,10 @@ function program2() {
     Powyższym sposobem nie stworzymy katalogu w katalogu.
     Aby powstała ścieżka jednym ze sposobów jest dodanie w opcjach recursive: true.
     Domyślnie jest to false. Takie podawanie całej ścieżki może być podatne na tworzenie błędów.
- */
+*/
 
 function program3() {
-    (async ()=>{
+    (async () => {
         try {
             await mkdirprom('./mega_k2', {
                 recursive: true, //stworzy za nas całą ścieżkę
@@ -51,10 +52,10 @@ function program3() {
 /*
     Zmiana nazwy pliku ale też przenoszenie pliku. W systemach unixowych, ale też w nodzie
     zmiana nazwy jest tym samym co przeniesienie pliku.
- */
+*/
 
 function program4() {
-    (async ()=>{
+    (async () => {
         try {
             await writeFileProm('./hello.txt', 'Hello, World!', 'utf8');
             await renameprom('./hello.txt', './renamedHello.txt')
@@ -69,8 +70,8 @@ function program4() {
 //program4()
 
 /*
-Przenosimy plik:
- */
+    Przenosimy plik:
+*/
 
 function program5() {
     (async ()=>{
@@ -88,17 +89,17 @@ function program5() {
 //program5()
 
 /*
-Globalna zmienna process
-Znajdują się w niej wszystkie informacje na temat środowiska, noda, etc, konsole, konto użytkownika...
-Jest elementem obiektu global.
+    Globalna zmienna process
+    Znajdują się w niej wszystkie informacje na temat środowiska, noda, etc, konsole, konto użytkownika...
+    Jest elementem obiektu global.
 
-argv - po wcześniejszych językach prog się przyjęło. Argument vector.
-Jest tablicą, zawiera pełną ścieżkę binarną do noda w wykorzystywanej wersji i to samo do pliku który jest wykonywany.
+    argv - po wcześniejszych językach prog się przyjęło. Argument vector.
+    Jest tablicą, zawiera pełną ścieżkę binarną do noda w wykorzystywanej wersji i to samo do pliku który jest wykonywany.
 
-Jeżeli wpiszemy w konsoli node index.js xyz sss fff, to argv dostanie kolejne elementy tablicy.
-W sytuacji kiedy w nazwie pliku lub katalogu mamy spację argument w linii komend możemy wpisać w nawiasie co rozwiąże problem.
+    Jeżeli wpiszemy w konsoli node index.js xyz sss fff, to argv dostanie kolejne elementy tablicy.
+    W sytuacji kiedy w nazwie pliku lub katalogu mamy spację argument w linii komend możemy wpisać w nawiasie co rozwiąże problem.
 
-Od drugiego miejsca w argv znajdują się rzeczy które tam przekazaliśmy sami. Możemy coś z nimi zrobić.
+    Od drugiego miejsca w argv znajdują się rzeczy które tam przekazaliśmy sami. Możemy coś z nimi zrobić.
  */
 
 function program6() {
@@ -113,11 +114,11 @@ function program6() {
 //program6()
 
 /*
-Program do zmiany nazw plików
- */
+    Program do zmiany nazw plików
+*/
 
 function renameProg() {
-    (async()=>{
+    (async () => {
         const oldFile = process.argv[2];
         const newFile = process.argv[3];
         try {
@@ -125,7 +126,9 @@ function renameProg() {
         } catch(err) {
             if (err.code === 'ENOENT') {
                 console.log('Given file name does not exist!')
-            } else {console.log(err)}
+            } else { 
+                console.log(err) 
+            }
         }
     })()
 }
@@ -133,22 +136,22 @@ function renameProg() {
 //renameProg()
 
 /*
-Usuwanie plików, katalogów i ich połączeń. Są do tego różne paczki, ale ostatnio pojawiło się fs.rm().
-Może ona usuwać pliki, katalogi oraz wszystko w środku. Inne to:
-1) fs.unlink()
-2) fs.rmdir()
-1 i 2 działają tak, że musimy zrobić unlink, potem jak już nie ma tam plików to rmdir, potem idziemy katalog wyżej itd, itp.
-Dlatego dobry jest ten nowy fs.rm(). Minusem jest to że rm powstał w wersji 14 noda.
- */
+    Usuwanie plików, katalogów i ich połączeń. Są do tego różne paczki, ale ostatnio pojawiło się fs.rm().
+    Może ona usuwać pliki, katalogi oraz wszystko w środku. Inne to:
+    1) fs.unlink()
+    2) fs.rmdir()
+    1 i 2 działają tak, że musimy zrobić unlink, potem jak już nie ma tam plików to rmdir, potem idziemy katalog wyżej itd, itp.
+    Dlatego dobry jest ten nowy fs.rm(). Minusem jest to że rm powstał w wersji 14 noda.
+*/
 
 function delProg() {
-    (async()=>{
+    (async () => {
         const selectedFile = process.argv[2];
         try {
-            await unlink(selectedFile)
+            await unlink(selectedFile);
         } catch(err) {
             if (err.code === 'ENOENT') {
-                console.log('Given file name does not exist!')
+                console.log('Given file name does not exist!');
             } else {console.log(err)}
         }
     })()
