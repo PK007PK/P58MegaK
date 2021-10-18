7c0ae9c89168d349b951cce625df8a2333e67f7e
Piotr Krasny <krasny.piotr@gmail.com>
2021-10-18 21:02:45 +0200

    Finish

--- routes/calc.js
+++ routes/calc.js
@@ -5,8 +5,7 @@ const calcRouter = express.Router();
 calcRouter
     .post('/check', (req, res) => {
         const {numberA, numberB} = req.body;
-        const divider = numberA % numberB === 0;
-        res.json({divider});
+        res.json({divider: numberA % numberB === 0});
     })
 
 
