b1c9c3957d93b4ff2521834bfa184ab80b924a14
Piotr Krasny <krasny.piotr@gmail.com>
2021-10-18 19:19:43 +0200

    Beginning of day 4

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const { readFile, writeFile } = require('fs').promises;
-const { voteRouter } = require('./routes/vote');
+const { calcRouter } = require('./routes/calc');
 function program1() {
     const app = express();
     app.use(express.json());
     app.use(express.static('public'));
-    app.use('/vote', voteRouter);
+    app.use('/calc', calcRouter);
     app.listen(3000);
 }
 program1()
