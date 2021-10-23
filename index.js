c5061de89e4741f6dc0a0a698e49b26525105514
Piotr Krasny <krasny.piotr@gmail.com>
2021-10-23 09:35:14 +0200

    End of day 332

--- index.js
+++ index.js
@@ -1,64 +1,28 @@
 /*
-Layout engines
-Silniki widoków. 
-
-To co jest obecnie w cookie, ten html jest niebezpieczne i nieczytelne. Aby tego uniknąć stosuje się
-języki szablonów. Jednym z najomopularniejszych w js jest Handlebars.
-
-Użyjemy express-handlebars..
-
-Przekazanie danych do handlebars:
-{{zmienna}}
-<h1>Witaj {{ imie }}</h1>
-
-Bardzo ważne i związane z bezpieczeństwem:
-1. Nikt Ci nie zrobi domyślnie ataku XSS. Sprawdźmy
-2. Nigdy nie generuj np. dunamicznie JS. Handlebars bezpiecznie traktuje tylko HTML.
-3. Nigdy nie przekazuj wszystkich danych przesłanych przez użytkownika. 
-
-Nie rób tego: ale jak musisz a to się zdarza:
-{{{zmienna}}} trzy nawiasy zamiast dwóch . Do tego możemy przekazać htmla. 
-
-Partiale
-Coś nie jest częścią szablonu, ale często się powtarza. Automatyzuje się to właśnie poprzez partiale. 
-Domyślnie znajdują się w views/partials
-
-Użycie partiala {{>nazwaPLiku}}
-
-Inne elementy i ułatwienia (helpers):
-komnetarze: {{!-- sdsd --!}} Taki nie wyląduje w html. Komentarz htmlowy użyty w handlebarsach będzie widoczny w źródle. 
-
-Pętla + co jeżeli nie ma wyników
-{{#each dates}}
-    <li>{{this}}</li>
-    {{else}}
-    <li>Brak wyników</li>
-{{/each}}
-
-Warunki . Powodują że w źródle strony nie będzie pustego taga i inne. 
-{{#if warunek}}
-    Tak
-{{/if}}
-
-Odwrotny
-{{#unless warunek}}
-    Nie
-{{/unless}}
-
-{{#if name}}
-    <h1>{{name}}</h1>
-    {{else}}
-    <h1>Brak imienia</h1>
-{{/if}}
-
-{{#unless name}}
-    <p>Brak imienia</p>
-{{/unless}}
-
-Ale już nie sprawdzimy if 10 > 0, te ify służą tylko do sprawdzania czy coś istnieje, czy jest puste. 
-Logikę więc przenosimy do miejsca gdzie renderujemy
+Konfigurator ciastek
+https://docs.google.com/document/d/1ItcySUO9ZdCPYuQ9x-qwi9DFpaNspa0O5z2XrVGBaEU/edit#
+https://drive.google.com/file/d/1AxGnkOOlBKWc2sHBmHFBlzYmztN9za3Y/view
 
 
-console.log tylko w hb -> {{log name age surname sex}}
-console.log tylko w hb -> {{log 'name' name 'age' age 'surname' surname 'sex' sex}}
 */
+
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const hbs = require('express-handlebars');
+
+const { homeRouter } = require('./routes/home');
+const { configuratorRouter } = require('./routes/configurator');
+const { orderRouter } = require('./routes/order');
+
+const app = express();
+app.use(express.json());
+app.use(express.static('public'));
+app.use(cookieParser());
+app.engine('.hbs', hbs({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+app.use('/', homeRouter);
+app.use('/configurator', configuratorRouter);
+app.use('/order', orderRouter);
+app.listen(3000);
+
