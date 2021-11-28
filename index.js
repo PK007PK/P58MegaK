27ae8ac5d7277df3286f3cac8fd661669c115a7a
Piotr Krasny <krasny.piotr@gmail.com>
2021-11-28 09:57:24 +0100

    Save

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+Nie ma czegoś takiego jak edytowanie nazwy, zmiana nazwy
+bazy danych w mySQL.
+
+Autoincrement może działać tylko na indeksie głównym.
+
+ctrl + enter -> zapisanie danych w tabeli
+insert -> wprowadzenie nowych danych
+*/
