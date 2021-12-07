a45796788e5130ad50b3ccf161b44359a8b09d68
Piotr Krasny <krasny.piotr@gmail.com>
2021-12-07 06:30:24 +0100

    Save

---
+++ index.js
@@ -0,0 +1,8 @@
+/*
+
+Wiele do wielu wymaga tabeli pośredniej
+
+INSERT INTO `students_courses`(`studentId`,`courseName`) VALUES ('1', 'Mega K')
+DELETE FROM `students_courses` WHERE `studentId` = 2 AND `courseName` = "Mega K"
+
+*/
