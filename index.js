02b8f0f3b3fbda781eb75175739387cb7a944d8e
Piotr Krasny <krasny.piotr@gmail.com>
2021-12-14 18:04:10 +0100

    Save

--- index.js
+++ index.js
@@ -2,15 +2,16 @@
 Zadanie: 
 1. Stworzyć nowy projekt, zainstalować mysql2 i skonfigurować połączenie.
 2. W bazie danych mega_courses wykonać za pomocą kodu następujące operacje:
-- wyświetlić wszystkioe kursy jakie mamy dostępne;
-- wybieramy wszystkich kursantów (id, imię, nazwisko), którzy mają min 18 lat
+a wyświetlić wszystkioe kursy jakie mamy dostępne;
+b wybieramy wszystkich kursantów (id, imię, nazwisko), którzy mają min 18 lat
 wraz z nazwami kursów na jakich są (Kursanci mogą się powtarzać, to jest ok)
-- usuwamy wszystkich kursantów, którzy są niepełnoletni i wyświetlamy ilu zostało 
+c usuwamy wszystkich kursantów, którzy są niepełnoletni i wyświetlamy ilu zostało 
 usuniętych, Korzystamy z parametrów;
-- Dodajemy nowego kursanta z kodu (korzystamy z parametrów) i wyświetlamy jego ID
+d Dodajemy nowego kursanta z kodu (korzystamy z parametrów) i wyświetlamy jego ID
 */
 
 const mysql = require('mysql2/promise');
+const {v4: uuid} = require('uuid'); //UUIDv4
 
 function zadanie() {
     const pool = mysql.createPool({
@@ -22,8 +23,47 @@ function zadanie() {
     });
 
     (async() => {
-        const [res] = await pool.execute('SELECT * FROM `courses`');
-        console.log(res);
+        //2a
+        const [courses] = await pool.execute('SELECT * FROM `courses`');
+        console.log(courses);
+        //2b
+        const [studentsAndCourses] = await pool.execute('SELECT `students`.`id`, `students`.`firstName`, `students`.`lastName`, `courses`.`name` FROM `students` JOIN `students_courses` ON `students`.`id` = `students_courses`.`studentId` JOIN `courses` ON `students_courses`.`courseName` = `courses`.`name` WHERE `students`.`age` >= 18');
+        console.log(studentsAndCourses);
+        //2c
+        /*
+        To zadziała, jeżeli w tabeli pośredniej ustawimy dla usuwania "cascade"
+        :age możemy zastosować, bo uruchomniliśmy sobie named placeholder
+        */
+        const {affectedRows: deletedStudentsUnderGivenAge} = (await pool.execute('DELETE FROM `students` WHERE `age` < :age', {age: 18}))[0];
+        console.log(deletedStudentsUnderGivenAge);
+
+        //2d
+
+        /*
+        Próba uzyskania uuid w ten sposób zawiodła, nie da się. Nie ma sprawy, bardziej zaawansowanymi narzędziami będziemy to robili i i tak nie będzie potrzebne. 
+        Tzn w bazę danych wstawiło ID, ale nie pobrało go od razu. Dlatego poniżej aby to uuid mieć wygeneruje go poprzez uuid. Równie dobrze mógłby najpierw dodać do bazy tworząc id
+        automatycznie, a potem moglibyśmy je pobrać aby wyświetlić. 
+
+        const {insertId} = (await pool.execute('INSERT INTO `students`(`firstName`, `lastName`, `age`, `addressStreet`) VALUES(:firstName, :lastName, :age, :addressStreet)', {
+            firstName: "Anna", 
+            lastName: "Kowalska", 
+            age: 22, 
+            addressStreet: "Pcimska 12",
+        }))[0];
+        console.log(insertId);
+        Ratujemy się instalując uuid
+        */
+
+        const newStudentId = uuid();
+        await pool.execute('INSERT INTO `students`(`id`, `firstName`, `lastName`, `age`, `addressStreet`) VALUES(:firstName, :lastName, :age, :addressStreet)', {
+            id: newStudentId,
+            firstName: "Anna", 
+            lastName: "Kowalska", 
+            age: 22, 
+            addressStreet: "Pcimska 12",
+        });
+        console.log(insertId);
+
         await pool.end();
     })();
 }
