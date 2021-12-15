55828147a7c8d95a2789fcbf4069880c8902fef6
Piotr Krasny <krasny.piotr@gmail.com>
2021-12-15 07:30:26 +0100

    Initial structure

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ Skorzystamy ze wzorców:
 - active record
 - repository
 */
+
+const { pool } = require("./utils/db");
+const {TodoRecord} = require("./records/todo/record");
+(async () => {
+
+    const firstTodoItem = new TodoRecord({
+        title: 'bbb 2',
+    });
+    const newId = await firstTodoItem.insert();
+    console.log(`New todo added with ID ${newId}`);
+    await pool.end();
+})()
--- records/todo/record.js
+++ records/todo/record.js
@@ -1,3 +1,31 @@
+//Active Record - sposób na robienie takich rekordów
+//w sposób aktywny
+
+const { pool } = require("../../utils/db");
+const {v4: uuid} = require('uuid');
 class TodoRecord {
-    
+    constructor(obj) {
+        if (obj.title.trim() < 5) {
+            throw new Error('To do title should be at last 5 characters.')
+        }
+
+        if (obj.title.length > 150) {
+            throw new Error('To do title should be at most 150 characters.')
+        }
+        this.id = obj.id;
+        this.title = obj.title;
+    }
+
+    async insert() {
+        this.id = this.id ?? uuid(); //null or undefined
+        await pool.execute('INSERT INTO `todos` VALUES(:id, :title)', {
+            id: this.id,
+            title: this.title,
+        });
+        return this.id;
+    }
+}
+
+module.exports = {
+    TodoRecord,
 }
