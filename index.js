6355af4edb3dda5a7795289cbd3408661255e487
Piotr Krasny <krasny.piotr@gmail.com>
2021-12-21 06:40:03 +0100

    Save

--- index.js
+++ index.js
@@ -132,5 +132,45 @@ Ale też od mongodb 3.5 możemy obsłużyć coś co przypomina joina sqlowego.
 }])
 
 
-#Użycie MongoDB w Node
+#Użycie MongoDB w Node:
+
+npm i mongodb
+
+const mongo = require('mongodb');
+const client = new mongo.MongoClient('mongodb://localhost:27017',
+    {
+        useNewUrlParser: true
+    }
+)
+client.connect();
 */
+const mongo = require('mongodb');
+
+function program1() {
+    /*
+    Poniżej string połączenia, to samo co podawaliśmy w Compass
+    */
+    const client = new mongo.MongoClient('mongodb://localhost:27017',
+    //Zazwyczaj zadziała też bez poniższego:
+        {useNewUrlParser: true}
+    );
+    (async() => {
+        await client.connect();
+        console.log("Baza połączona");
+        await client.close();
+    })();
+}
+//program1();
+
+function program2() {
+    const client = new mongo.MongoClient('mongodb://localhost:27017');
+    (async() => {
+        await client.connect();
+        const db = client.db('megak_test');
+        const cars = db.collection('cars');
+        const users = db.collection('users');
+        console.log(users.find());
+        await client.close();
+    })();
+}
+program2();
