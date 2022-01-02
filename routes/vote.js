18909f1f27029c98aa234b3f239bcea0f4052a2b
Piotr Krasny <krasny.piotr@gmail.com>
2022-01-02 15:12:18 +0100

    Save

--- routes/vote.js
+++ routes/vote.js
@@ -11,22 +11,25 @@ const votes = {
 const ipRestrict = new IpRestrict();
 
 voteRouter
-//ten check musi być pierwszy żeby się nie dodał jako głos. 
-.get('/check', async (req,res) => {
-    const info = Object.entries(votes).map( ar => `Votes on ${ar[0]}: ${ar[1]}`).join('<br>');
-    res.send(info);
-})
-.get('/:voteName', async (req,res) => {
-    if (!ipRestrict.check(req.ip)) {
-        res.status(403).send("Głos oddano już wcześniej");
-        return;
-    };
-    const {voteName} = req.params;
-    if (typeof votes[voteName] === "undefined") {
-        votes[voteName] = 0;
-    };
-    votes[voteName]++;
-})
+    //ten check musi być pierwszy żeby się nie dodał jako głos. 
+    .get('/check', async (req,res) => {
+        const info = Object.entries(votes).map( ar => `Votes on ${ar[0]}: ${ar[1]}`).join('<br>');
+        res.send(info);
+    })
+    
+    .get('/:voteName', async (req,res) => {
+        if (!ipRestrict.check(req.ip)) {
+            res.status(403).send("Głos oddano już wcześniej");
+            return;
+        };
+
+        const {voteName} = req.params;
+
+        if (typeof votes[voteName] === "undefined") {
+            votes[voteName] = 0;
+        };
+        votes[voteName]++;
+    })
 
 module.exports = {
     voteRouter,
