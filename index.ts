a21dc0d90c43545d4f7fcc15bddbcfe7c8841a1e
Piotr Krasny <krasny.piotr@gmail.com>
2022-02-12 18:33:30 +0100

    Show errors using errors view

--- index.ts
+++ index.ts
@@ -9,7 +9,10 @@ import { arenaRouter } from './routers/arena';
 import { hallOfFameRouter } from './routers/hall-of-fame';
 // import { handleError } from './utils/errors';
 import './utils/db'; //Robimy pusty import aby połączenie z bazą danych się otworzyło
+import { handleError } from './utils/errors';
 
+
+//#App configurations
 const app = express();
 app.use(methodOverride('_method'));
 app.use(urlencoded({
@@ -22,11 +25,14 @@ app.engine('.hbs', engine({//Zmieniło się w stosunku do poprzednich wersji
 })); 
 app.set('view engine', '.hbs');
 
+//#All routes
 //Routy powinniśmy dodawać po konfiguracji ekspresu, a przed obsługą błędów
 app.use('/', homeRouter);
 app.use('/warrior', warriorRouter);
 app.use('/arena', arenaRouter);
 app.use('/hall-of-fame', hallOfFameRouter);
 
-//app.use(handleError)
+//#Handling errors
+app.use(handleError)
+
 app.listen(3000, ()=>console.log("Listening on http://localhost:3000"));
--- utils/errors.ts
+++ utils/errors.ts
@@ -5,7 +5,18 @@ export class ValidationError extends Error {}
 export const handleError = (err: Error, req: express.Request, res: express.Response, next: express.NextFunction): void => {
     console.error(err);
     res
+        /*
+            Sprawdzamy poniżej, czy błąd jest naszym błędem. Czyli że ktoś coś źle podał. Jeżeli tak to 400. 
+            400 oznacza bad request: źle podajesz..
+            Jeżeli nie 400 to 500 - to my coś spieprzylismy
+        */
         .status(err instanceof ValidationError ? 400 : 500)
+        /*
+            Dalej renderujemy stronę error i przekazujemy jej message. 
+            Jeżeli nasz błąd, to przekazujemy naszą wiadomość. 
+            Jeżeli nie nasz to przekazujemy ogólną informację że 
+            to problem po stronie apki. 
+        */
         .render('error', {
             message: err instanceof ValidationError ? err.message : 'Przepraszamy, spróbuj ponownie za kilka minut.',
         });
