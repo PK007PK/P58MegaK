2640731725ecaee0b66dcf08fb531830a5ddfa86
Piotr Krasny <krasny.piotr@gmail.com>
2022-02-12 20:04:29 +0100

    Improve variors list in hall of fame

--- routers/hall-of-fame.ts
+++ routers/hall-of-fame.ts
@@ -5,7 +5,15 @@ export const hallOfFameRouter = Router(); //Ma być bez new, tak to sobie wymyś
 
 hallOfFameRouter
     .get('/', async (req, res) => {
-        const warriors = await WarriorRecord.listTop(3);
+        const warriors = (
+            await WarriorRecord.listTop(10)
+        ).map((warrior, index) => {
+            return {
+                place: index + 1,
+                warrior,
+            };
+        });
+
         res.render('hall-of-fame/list', {
             warriors,
         });
