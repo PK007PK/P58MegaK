e589c5cf423791c8ccb5294fad61df6c81978e68
Piotr Krasny <krasny.piotr@gmail.com>
2022-03-29 21:38:09 +0200

    Change response to json in giftRouter

--- index.ts
+++ index.ts
@@ -29,6 +29,6 @@ app.use('/gift', giftRouter);
 
 app.use(handleError);
 
-app.listen(3000, '0.0.0.0', () => {
-    console.log('Listening on http://localhost:3000');
+app.listen(3001, '0.0.0.0', () => {
+    console.log('Listening on http://localhost:3001');
 });
